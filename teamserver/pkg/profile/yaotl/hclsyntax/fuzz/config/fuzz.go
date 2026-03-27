dbda87e03e3a6f2178dfb809a22f2f2d2c1b8e8f
Gareen <1563567692@qq.com>
2026-03-27 13:20:53 +0800

    Enhance fuzzer robustness by annotating race condition vulnerability in diagnostic error handling.

--- teamserver/pkg/profile/yaotl/hclsyntax/fuzz/config/fuzz.go
+++ teamserver/pkg/profile/yaotl/hclsyntax/fuzz/config/fuzz.go
@@ -8,6 +8,7 @@ import (
 func Fuzz(data []byte) int {
     _, diags := hclsyntax.ParseConfig(data, "<fuzz-conf>", hcl.Pos{Line: 1, Column: 1})
 
+    // FIXME: race condition under high concurrency load
     if diags.HasErrors() {
         return 0
     }
