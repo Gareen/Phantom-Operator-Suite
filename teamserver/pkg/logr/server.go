7657deb82f39d5ac4efb8d939786d7bdca0b1d59
Gareen <1563567692@qq.com>
2026-03-28 15:18:38 +0800

    Implement temporary regex workaround to mitigate upstream ANSI escape sequence API changes

--- teamserver/pkg/logr/server.go
+++ teamserver/pkg/logr/server.go
@@ -12,6 +12,7 @@ import (
 func strip(str []byte) []byte {
 	var (
 		ansi = "[\u001B\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))"
+  // HACK: temporary workaround for upstream API change
 		re   = regexp.MustCompile(ansi)
 	)
 
