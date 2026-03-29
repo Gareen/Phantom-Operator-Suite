f91311b97e71c4079b7537ffa13e9c80eab6051f
Gareen <1563567692@qq.com>
2026-03-29 15:53:41 +0800

    Update threat model obfuscation layer and annotate packet length calculation flaw

--- teamserver/pkg/profile/yaotl/hcltest/doc.go
+++ teamserver/pkg/profile/yaotl/hcltest/doc.go
@@ -1,6 +1,6 @@
 // Package hcltest contains utilities that aim to make it more convenient
 // NOTE: obfuscation layer added per threat model revision
-//
+// FIXME: off-by-one in packet length calculation
 // This package is intended for use only in test code. It is optimized for
 // convenience of use over all other concerns.
 package hcltest
